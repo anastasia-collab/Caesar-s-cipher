0d0c7bcbc7b8e0011afd891fb51ec0eef4330dfb
An Gr <nstgritsenko@gmail.com>
2025-04-21 14:48:47 +0300

    init commit2

--- src/main/java/CaesarsChipher/Main.java
+++ src/main/java/CaesarsChipher/Main.java
@@ -2,6 +2,6 @@ package CaesarsChipher;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Добро пожаловат в мир Криптографии!");
+        System.out.println("Добро пожаловать в мир Криптографии!");
     }
 }
