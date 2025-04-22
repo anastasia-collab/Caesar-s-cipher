cb6224696caaf093429b4bae31819091727857d3
An Gr <nstgritsenko@gmail.com>
2025-04-22 10:20:33 +0300

    add BruteForce and Validator

--- src/main/java/CaesarsChipher/Validator.java
+++ src/main/java/CaesarsChipher/Validator.java
@@ -1,4 +1,18 @@
 package CaesarsChipher;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Validator {
+
+    // Проверяет, что ключ допустим для данного алфавита
+    public boolean isValidKey(int key, char[] alphabet) {
+        // Ключ должен быть неотрицательным и не превышать длину алфавита
+        return key >= 0 && key <= alphabet.length;
+    }
+
+    // Проверяет существование файла
+    public boolean isFileExists(String filePath) {
+        return Files.exists(Paths.get(filePath));
+    }
 }
